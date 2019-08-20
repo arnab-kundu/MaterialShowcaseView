5f072d4f28fdb7b897e91085ec7d86b1574d7e66
amanc <aman.choudhury@e-arc.com>
2019-08-20 12:37:14 +0530

    Switch added which will decide to show the tutorial or not

--- sample/src/main/java/uk/co/deanwild/materialshowcaseviewsample/MainActivity.java
+++ sample/src/main/java/uk/co/deanwild/materialshowcaseviewsample/MainActivity.java
@@ -2,15 +2,23 @@ package uk.co.deanwild.materialshowcaseviewsample;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import uk.co.deanwild.materialshowcaseview.MaterialShowcaseView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    SharedPreferences sharedPreferences;
+    Switch wantTutorial;
+    SharedPreferences.Editor editor;
+    boolean wantToSeeTutorial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +34,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button.setOnClickListener(this);
         button = findViewById(R.id.btn_reset_all);
         button.setOnClickListener(this);
+        wantTutorial=findViewById(R.id.wantTutorial);
 
+        sharedPreferences=getSharedPreferences("ARC",MODE_PRIVATE);
+        editor=sharedPreferences.edit();
+
+        wantToSeeTutorial=sharedPreferences.getBoolean("wantTutorial",false);
+        if(wantToSeeTutorial)
+            wantTutorial.setChecked(true);
+
+        wantTutorial.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                    editor.putBoolean("wantTutorial", true);
+                else
+                    editor.putBoolean("wantTutorial", false);
+                editor.commit();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        wantToSeeTutorial=sharedPreferences.getBoolean("wantTutorial",false);
+        Toast.makeText(this, String.valueOf(wantToSeeTutorial), Toast.LENGTH_SHORT).show();
     }
 
     @Override
